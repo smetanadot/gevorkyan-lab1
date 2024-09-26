7ebe32b00973b6dab1dcd7328101aca3919d676e
smetanadot <7631000@gmail.com>
2024-09-26 15:34:20 +0300

    Почти доделал!!

--- gevorkyan-lab1/gevorkyan-lab1/gevorkyan-lab1.cpp
+++ gevorkyan-lab1/gevorkyan-lab1/gevorkyan-lab1.cpp
@@ -1,7 +1,4 @@
-﻿// эффективность доолжна вводиться
-// функции от копипаста
-// удалить воркинг
-// редактировать колво рабочих фабрик
+﻿
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -9,18 +6,18 @@ using namespace std;
 
 struct pipeline // pipe
 {
-	string pipename;
-	int pipelength;
-	int pipediameter;
-	bool piperepair;
+	string pipename = "None";
+	int pipelength = 0;
+	int pipediameter = 0;
+	bool piperepair = false;
 };
 
 struct compressorstation //cs
 {
-	string csname;
-	int csshop;
-	int csworkshop;
-	double csefficiency;
+	string csname = "None";
+	int csshop = 0;
+	int csworkshop = 0;
+	double csefficiency = 0;
 };
 
 bool verification()
@@ -158,8 +155,8 @@ void menu()
 		"3. Show all objects\n" <<
 		"4. Edit pipeline\n" <<
 		"5. Edit cs\n" <<
-		"6. Save\n" <<
-		"7. Load\n" << endl;
+		"6. Save \n" << 
+		"7. Load" << endl;
 };
 
 void show_all_objects(const compressorstation& c, const pipeline& p)
@@ -191,7 +188,77 @@ void show_all_objects(const compressorstation& c, const pipeline& p)
 	}
 }
 
+void save_p(ofstream& fout, const pipeline& p)
+{
+	if (!(p.pipename == ""))
+	{
+		fout << p.pipename << endl;
+		fout << p.pipelength << endl;
+		fout << p.pipediameter << endl;
+		fout << p.piperepair << endl;
+	}
+	else
+	{
+		fout << "There is none pipe" << endl;
+	}
+}
 
+void save_c(ofstream& fout, const compressorstation& c)
+{
+	if (!(c.csname == ""))
+	{
+		fout << c.csname << endl;
+		fout << c.csshop << endl;
+		fout << c.csworkshop << endl;
+		fout << c.csefficiency << endl;
+	}
+	else
+	{
+		fout << "There is none CS" << endl;
+	}
+}
+
+bool load_p(ifstream& fin, pipeline& p)
+{
+	if (!(getline(fin, p.pipename)))
+	{
+		return false;
+	}
+	if (!(fin >> p.pipelength))
+	{
+		return false;
+	}
+	if (!(fin >> p.pipediameter))
+	{
+		return false;
+	}
+	if (!(fin >> p.piperepair))
+	{
+		return false;
+	}
+	return true;
+}
+	
+bool load_c(ifstream& fin, compressorstation& c)
+{
+	if (!(getline(fin, c.csname)))
+	{
+		return false;
+	}
+	if (!(fin >> c.csshop))
+	{
+		return false;
+	}
+	if (!(fin >> c.csworkshop))
+	{
+		return false;
+	}
+	if (!(fin >> c.csefficiency))
+	{
+		return false;
+	}
+	return true;
+}
 
 int main()
 {
@@ -200,52 +267,77 @@ int main()
 	compressorstation c;
 	pipeline p;
 
-	ofstream save("save.txt");
-
-
 	while (true)
 	{
-
 		menu();
 		cin >> choice;
 		switch (choice)
 		{
 		case 0: // escape
-			break;
+			return 0;
 
 		case 1: // add new pipeline
 
 			add_pipe(p);
 			
-			continue;
+			break;
 
 		case 2: // add new cs
 
 			add_cs(c);
 			
-			continue;
+			break;
 
 		case 3: // show all objects
 
 			show_all_objects(c,p);
 
-			continue;
+			break;
 
 		case 4: // edit pipe
 			
 			edit_pipe(p);
 
-			continue;
+			break;
 		case 5: // edit cs
 
 			edit_cs(c);
 
-			continue;
-			
-		case 6: // save
-			continue;
-		case 7: // load
 			break;
+			
+		case 6: // save 
+		{
+			ofstream fout("save_data.txt");
+			if (fout.is_open())
+			{
+				save_p(fout, p);
+				save_c(fout, c);
+				fout.close();
+				cout << "Saved" << endl;
+			}
+			else
+			{
+				cout << "Failed to save" << endl;
+			}
+			break;
+		}
+		case 7: // load
+		{
+			ifstream fin("save_data.txt");
+			if (fin.is_open())
+			{
+				load_p(fin, p);
+				load_c(fin, c);
+				fin.close();
+				cout << "Loaded" << endl;
+			}
+			else
+			{
+				cout << "Failed to load" << endl;
+			}
+			break;
+		}
+		
 		default: // unexpected error
 			break;
 		}
